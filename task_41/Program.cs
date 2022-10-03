6416a62497e44fff09e359956f546559bf036e7c
tim24ktk <tim24ktk@mail.ru>
2022-10-04 00:26:40 +0300

    homework_6

---
+++ task_41/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+    0, 7, 8, -2, -2 -> 2
+
+    1, -7, 567, 89, 223-> 3
+*/
+
+string ReadData()
+{
+    Console.WriteLine("Введите числа через запятую: ");
+    string numString = Console.ReadLine() ?? "";
+    return numString;
+}
+
+int CalculateNumbers(string line)
+{
+    int count = 0;
+
+    string[] words = line.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+    for (int i = 0; i < words.Length; i++)
+    {
+
+        if (int.Parse(words[i]) > 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string readData = ReadData();
+int count = CalculateNumbers(readData);
+
+if (count > 0)
+    Console.WriteLine($"Среди введённых пользователем чисел {count} больше 0");
+else
+    Console.WriteLine($"Среди введённых пользователем нет чисел больше 0");
---
+++ task_43/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+    Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+    значения b1, k1, b2 и k2 задаются пользователем.
+    b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double ReadData(string line)
+{
+    Console.WriteLine(line);
+    double number = double.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+void FindIntersection(double b1, double k1, double b2, double k2)
+{
+    if (b2 != b1 && k1 != k2)
+    {
+        // k1 * x + b1 - (k2 * x + b2) = 0; k1 * x + b1 - k2 * x - b2 = 0; k1 * x - k2 * x = b2 - b1; x * (k1 -k2) = b2 -b1; x = (b2 - b1) / (k1 - k2)
+
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k2 * x + b2;
+        Console.WriteLine($"Координаты точек пересечения 2-х прямых ({x}, {y})");
+    }
+    else
+    {
+        Console.WriteLine($"У прямых нет точек пересечения! Они либо параллельны, либо совпадают!");
+    }
+}
+
+double b1 = ReadData("Введите значение b1: ");
+double k1 = ReadData("Введите значение k1: ");
+double b2 = ReadData("Введите значение b2: ");
+double k2 = ReadData("Введите значение k2: ");
+
+FindIntersection(b1, k1, b2, k2);
